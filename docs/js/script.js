e8910fe0992c176b866c09bd3a3fb146a787731b
sharko <m.pavlovets@mail.ru>
2021-07-16 00:06:31 +0300

    1-5last

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,6 +1,25 @@
-const swiper = new Swiper('.swiper-container', {
-    pagination: {
+new Swiper('.image-slider', {
+  // стрелки
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+  },
+  // навигация
+  // буллеты, текущее положение, прогрессбар
+  pagination: {
       el: '.swiper-pagination',
-      type: 'bullets',
-    },
-  });
+      // буллеты
+      clickable: true,
+      dinamicbullets: true,
+  },
+  autoHeight: true,
+  loop:true,
+//    автопрокрутка
+  autoplay: {
+      delay: 2500,
+      disableOnInteraction: false
+  },
+      speed: 1000,
+  
+})
+
